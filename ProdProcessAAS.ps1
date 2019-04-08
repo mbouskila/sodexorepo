c7d98d63467a190e51d5d45ab71940c3a503dae3
mbouskila <mbouskila@hotmail.com>
2019-04-09 00:23:46 +0200

    Update ProdProcessAAS.ps1

--- ProdProcessAAS.ps1
+++ ProdProcessAAS.ps1
@@ -1,4 +1,4 @@
 $AzureCred = Get-AutomationPSCredential -Name "SodexoASRefreshCred"
 Add-AzureAnalysisServicesAccount -RolloutEnvironment 'northeurope.asazure.windows.net' -ServicePrincipal -Credential $AzureCred -TenantId "a289d6c2-3b1f-4bc4-8fa0-6866ff300052"
 
-Invoke-ProcessASDatabase -server "asazure://northeurope.asazure.windows.net/azieas1zsx197" -DatabaseName "SodexoAS" -RefreshType Full -Verbose
+Invoke-ProcessASDatabase -server "asazure://northeurope.asazure.windows.net/azieas1zsx198" -DatabaseName "SodexoAS" -RefreshType Full -Verbose
